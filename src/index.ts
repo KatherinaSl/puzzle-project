3289822c2809a30ea39b7e3deb3bbd919027e163
KatherinaSl <106989490+KatherinaSl@users.noreply.github.com>
2024-06-03 14:53:53 +0500

    Revert "feat: RSS-PZ-04: Restrict Main Functionality to Logged-In Users and I…" (#5)
    This reverts commit d2ba02b44a7e77ffe0113aeefd89710fe90feb4f.

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import App from './spa/app';
-import './style.scss';
-// import ionicons from '@ionicons';
-
-const app = new App();
-app.start();
--- src/spa/util/element-creator.ts
+++
@@ -1,7 +0,0 @@
-export default function createHTMLElement(tagName: string, className?: string): HTMLElement {
-    const element = document.createElement(tagName);
-    if (className) {
-        element.classList.add(className);
-    }
-    return element;
-}
